616828f5878a793baff1db909676a8809ce83c79
Luke Hammer <textchimp@gmail.com>
2017-06-07 14:29:05 +1000

    Wk 04 Day 03: sinatra intro

---
+++ week4/sinatra/app.rb
@@ -0,0 +1,83 @@
+require "sinatra"
+require "sinatra/reloader"
+require "pry"
+
+def html_header( colour )
+    "<html>
+  <head>
+  </head>
+  <body style=\"background-color: #{ colour };\">
+  <h1>Welcome to the general-purpose calculator</h1>"
+end
+
+$output_start = "<html>
+<head>
+</head>
+<body>
+<h1>Welcome to the general-purpose calculator</h1>"
+
+$output_end = "</body></html>"
+
+get "/hello" do
+  # matches the localhost:4567/hello URL
+  a = 10
+  "Hello World! a = #{ a }"
+end
+
+get "/colour/:col" do
+  output = html_header( params[ "col" ] )
+  output += "Here's your fancy colour #{ params["col"] }"
+  output += $output_end
+end
+
+get "/hello/:name" do  # /hello/edgy
+  # binding.pry
+
+  # params[ "name" ] = "edgy";
+  #
+  # if params[ "name" ] == "edgy"
+  #   "get lost jerk!"
+  # else
+  #   "Hello friend"
+  # end
+
+  "Hello, #{ params["name"] }"
+
+end
+
+get "/hello/:first_name/:surname/:age" do
+  "Good day to you, #{ params["first_name"] } #{ params["surname"] }  - you are #{ params["age"] } years old."
+end
+
+get "/add/:x/:y" do
+  result = params["x"].to_i + params["y"].to_i
+  "The sum of #{ params["x"] } and #{ params["y"] } is #{ result }"
+end
+
+get "/mult/:x/:y" do
+  result = params["x"].to_i * params["y"].to_i
+  "The product of #{ params["x"] } and #{ params["y"] } is #{ result }"
+end
+
+get "/calc/:op/:x/:y" do
+  x = params["x"].to_i
+  y = params["y"].to_i
+  op = params["op"]
+
+  # x + y
+  # x.+( y )
+  result = x.send(op, y)  # x.+( y )  or x.*( y ) etc
+
+
+  $output_start + "The result of #{x} #{op} #{y} = #{result}" + $output_end
+
+end
+
+
+get "/good/bye" do
+  "<h1>Goodbye I guess</h1>"
+end
+
+get "/" do
+  "Hello World, but from the home page"
+end
