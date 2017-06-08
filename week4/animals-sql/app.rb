2f99817314355fec25ad48bbf885740adaf9d87e
Luke Hammer <textchimp@gmail.com>
2017-06-08 15:08:15 +1000

    Wk 04 Day 03: SQL horror

--- week4/animals-sql/app.rb
+++ week4/animals-sql/app.rb
@@ -11,14 +11,61 @@ require "sqlite3"
 # POST to action="/animals" from a form
 # /animals           => params["species"], params["legs"]
 
-post "/animals/" do
-  binding.pry
+
+def query_db( sql )
+  # Create a connection to the database
+  db = SQLite3::Database.new 'database.db'
+
+  # Ask for the information in a nicer format
+  db.results_as_hash = true
+
+  # Show the SQL that was generated in the server output
+  puts sql
+
+  # Execute a line of SQL and store the result
+  result = db.execute sql
+  db.close
+
+  result
+end
+
+get "/animals" do
+
+  # get every row from the animals table
+  @animals = query_db "SELECT * FROM animals;"
+
+  erb :index
 
 end
 
-
+# show the New form for an Animal
 get "/animals/new" do
-    erb :new
+  erb :new
+end
+
+
+post "/animals" do
+
+# params["species"]
+# params["image"]
+
+  sql = "
+INSERT INTO animals( species, image, description, legs ) VALUES(
+  '#{ params["species"] }',
+  '#{ params["image"] }',
+  '#{ params["description"] }',
+   #{ params["legs"] }
+);"
+
+  # # Create a connection to the database
+  # db = SQLite3::Database.new 'database.db'
+  # # Execute a line of SQL and store the result
+  # result = db.execute sql
+
+  query_db( sql )
+
+  "It worked! SQL: " + sql
+
 end
 
 
