63bb82d819b535b8403bac208b3c779fef5e086a
Luke Hammer <textchimp@gmail.com>
2017-06-13 14:49:04 +1000

    MoMA commit 5, update

--- week5/moma-app/app/controllers/artists_controller.rb
+++ week5/moma-app/app/controllers/artists_controller.rb
@@ -1,15 +1,29 @@
 
 class ArtistsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    # raise 'hell'
+    artist = Artist.create({
+      name: params["name"],
+      nationality: params["nationality"],
+      dob: params["dob"],
+      period: params["period"],
+      image: params["image"]
+    })
+
+    redirect_to "/artists/#{ artist.id }"  # go to show page
+  end
+
   def index
     @artists = Artist.all
   end
 
   def show
-
     # id = params["id"]
     @artist = Artist.find params["id"]
-
   end
 
 
--- week5/moma-app/config/routes.rb
+++ week5/moma-app/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/artists" => "artists#index"
 
-  get "/artists/:id" => "artists#show" 
+
+  get "/artists/new"  => "artists#new" # empty form for new artist
+  post "/artists/new" => "artists#create"  # handle submitted form and create new row in table
+
+  # 'R' - Read: index and show
+  get "/artists" => "artists#index" # list of all artists in table
+  get "/artists/:id" => "artists#show" # details for one artist
 
 end
