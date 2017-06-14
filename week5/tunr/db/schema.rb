90253951caa204736dcd3a2f6679e799c02c59e1
Luke Hammer <textchimp@gmail.com>
2017-06-14 16:59:27 +1000

    user CRUD done

--- week5/tunr/db/schema.rb
+++ week5/tunr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614050854) do
+ActiveRecord::Schema.define(version: 20170614060243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170614050854) do
     t.boolean  "is_fire"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "mixtapes_songs", force: :cascade do |t|
@@ -59,6 +60,14 @@ ActiveRecord::Schema.define(version: 20170614050854) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "album_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text     "email"
+    t.text     "name"
+    t.text     "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
