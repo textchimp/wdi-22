9407dad5a2e7dfc033adedbd8fcd67669a7d161a
Luke Hammer <textchimp@gmail.com>
2017-06-15 12:51:42 +1000

    tunr sessions and authentication 2

--- week5/tunr/app/controllers/session_controller.rb
+++ week5/tunr/app/controllers/session_controller.rb
@@ -3,7 +3,18 @@ class SessionController < ApplicationController
   end
 
   def create
-    raise 'hell'
+    user = User.find_by email: params[:email]
+
+    if user.present? and user.authenticate params[:password]
+      # success!
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      # mismatch/bad credentials
+      flash[:error] = "Incorrect email address and/or password"
+      redirect_to login_path
+    end
+
   end
 
   def destroy
