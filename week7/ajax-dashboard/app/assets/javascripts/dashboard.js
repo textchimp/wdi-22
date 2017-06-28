216e162692f4c1ad4ae025d994258b0b0a788614
Luke Hammer <textchimp@gmail.com>
2017-06-28 14:06:38 +1000

    wk 07 day 03: rails ajax 02

--- week7/ajax-dashboard/app/assets/javascripts/dashboard.js
+++ week7/ajax-dashboard/app/assets/javascripts/dashboard.js
@@ -2,22 +2,26 @@ $(document).ready(function(){
 
   console.log('Loaded.');
 
-  $.ajax({
-    url: '/info-api'
-  })
-  .done(function(res){
+  var getDashboardData = function(){
+    $.ajax({
+      url: '/info-api'
+    })
+    .done(function(res){
 
-    $('#up').html( res.uptime );
+      $('#up').html( res.uptime );
 
-    $('#catname_title').html( res.catname.title );
-    $('#catname_first').html( res.catname.first_name );
-    $('#catname_last').html( res.catname.surname );
+      $('#catname_title').html( res.catname.title );
+      $('#catname_first').html( res.catname.first_name );
+      $('#catname_last').html( res.catname.surname );
 
-    $('#hog').html( res.cpuhog );
+      $('#hog').html( res.cpuhog );
 
-  })
-  .fail(function(xhr, status, err){
-    console.log(xhr, status, err);
-  });
+    })
+    .fail(function(xhr, status, err){
+      console.log(xhr, status, err);
+    });
+  };
+
+  setInterval(getDashboardData, 3000);
 
 });
