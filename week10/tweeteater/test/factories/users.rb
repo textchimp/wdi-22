d71454b75b6de4d8686ccad3606d2f333f08ccee
Luke Hammer <textchimp@gmail.com>
2017-07-18 15:17:30 +1000

    clarity

--- week10/tweeteater/test/factories/users.rb
+++ week10/tweeteater/test/factories/users.rb
@@ -4,11 +4,15 @@ FactoryGirl.define do
     # email Faker::Internet.email
     # name Faker::Name.name_with_middle
 
+    # Make sure that each new user we create gets a new name and email address
     f.sequence( :email ){ Faker::Internet.email }
     f.sequence( :name  ){ Faker::Name.name_with_middle }
 
+    # Create a sub-factory which also does what its parent factory does
+    # (i.e. create a user), but then also creates tweets for that user afterwards
     factory :user_with_tweets do
       after(:create) do |u|
+        # When we've finished making a user
         FactoryGirl.create_list :tweet, Random.rand(10..200), :user => u
       end
     end # :user_with_tweets
